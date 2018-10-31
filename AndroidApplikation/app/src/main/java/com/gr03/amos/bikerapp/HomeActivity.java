ff14f19926a6febeaf403d1cbe76d75dcde9ca1d
Waleed Butt <waleedshahdin@gmail.com>
2018-10-31 01:01:47 +0100

    Created the home page, just the design

---
+++ AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.gr03.amos.bikerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
