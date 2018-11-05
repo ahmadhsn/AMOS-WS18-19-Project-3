e69ae7a828496ce5786ab244ef93c723ad7a0ac9
Waleed Butt <waleedshahdin@gmail.com>
2018-11-05 18:18:46 +0100

    minor tweak

--- AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/SignUpActivity.java
+++ AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/SignUpActivity.java
@@ -19,6 +19,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        businessName = findViewById(R.id.business_name);
     }
 
     public void onRadioButtonClicked(View view) {
