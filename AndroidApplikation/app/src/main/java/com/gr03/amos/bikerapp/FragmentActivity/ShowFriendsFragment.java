ec15e1c4f6f1e249359f02a169ab4af17c9e3067
anika.apel <32097938+anikaapel@users.noreply.github.com>
2018-12-12 14:45:59 +0100

    changed title in friendslist

--- AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/FragmentActivity/ShowFriendsFragment.java
+++ AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/FragmentActivity/ShowFriendsFragment.java
@@ -1,21 +1,29 @@
 package com.gr03.amos.bikerapp.FragmentActivity;
 
+import android.app.Activity;
+import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gr03.amos.bikerapp.Models.Event;
 import com.gr03.amos.bikerapp.Models.Friend;
 import com.gr03.amos.bikerapp.R;
 import com.gr03.amos.bikerapp.RecylerViewAdapter.ShowFriendsListRecyclerViewAdapter;
 import com.gr03.amos.bikerapp.Requests;
+import com.gr03.amos.bikerapp.SearchUserActivity;
 import com.gr03.amos.bikerapp.ShowEventActivity;
 import com.gr03.amos.bikerapp.ShowEventRecylerViewAdapter;
 
@@ -40,6 +48,10 @@ public class ShowFriendsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_show_friends, container, false);
 
+        Activity currActivity = getActivity();
+        //Set Title to My Friends
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.title_fragment_friendlist);
 
         //TODO use sessionID instead
         int userId = 1;
