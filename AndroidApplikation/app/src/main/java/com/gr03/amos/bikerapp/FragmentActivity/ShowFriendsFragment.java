3e850f35cccd00d3321785d69f35faa5d410c7d0
anika.apel <32097938+anikaapel@users.noreply.github.com>
2018-12-18 11:56:39 +0100

    changed to searchview

--- AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/FragmentActivity/ShowFriendsFragment.java
+++ AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/FragmentActivity/ShowFriendsFragment.java
@@ -1,6 +1,7 @@
 package com.gr03.amos.bikerapp.FragmentActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.gr03.amos.bikerapp.Adapters.UserAdapter;
@@ -35,11 +37,13 @@ import io.realm.OrderedRealmCollection;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-public class ShowFriendsFragment extends Fragment {
+public class ShowFriendsFragment extends Fragment implements SearchView.OnQueryTextListener {
+    Context context;
+
     RecyclerView showFriendsRecyclerView;
     ShowFriendsListRecyclerViewAdapter showFriendsListRecyclerViewAdapter;
 
-    EditText userName;
+    SearchView searchUser;
     ListView listView;
 
     public ShowFriendsFragment() {
@@ -55,8 +59,12 @@ public class ShowFriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_show_friends, container, false);
+        context = container.getContext();
 
-        userName = view.findViewById(R.id.userName);
+        searchUser = view.findViewById(R.id.searchUser);
+        searchUser.setOnQueryTextListener(this);
+
+        //userName = view.findViewById(R.id.userName);
         listView = view.findViewById(R.id.user_result);
         Activity currActivity = getActivity();
         //Set Title to My Friends
@@ -75,80 +83,83 @@ public class ShowFriendsFragment extends Fragment {
         showFriendsRecyclerView.setAdapter(showFriendsListRecyclerViewAdapter);
 
         //set onClickListener for Button
-        Button btSearch = (Button) view.findViewById(R.id.searchUsers);
-        btSearch.setOnClickListener(new SearchOnClickListener());
+        //TODO remove if successful
+        //Button btSearch = (Button) view.findViewById(R.id.searchUsers);
+        //btSearch.setOnClickListener(new SearchOnClickListener());
         return view;
     }
 
-    class SearchOnClickListener implements View.OnClickListener {
+    private void listUsers(JSONArray users) {
+        ArrayList<BasicUser> arrayUsers = new ArrayList<>();
+        OrderedRealmCollection<Friend> friends = ShowFriendsFragment.this.showFriendsListRecyclerViewAdapter.getData();
 
-        @Override
-        public void onClick(View v) {
-            String url = "searchUser";
+        for (int i = 0; i < users.length(); i++) {
 
             try {
-                JSONObject response;
-
-                JSONObject request = new JSONObject();
-                request.put("id", SaveSharedPreference.getUserID(v.getContext()));
-                request.put("input",userName.getText());
-
-                FutureTask<String> task = new FutureTask(new Callable<String>() {
-                    public String call() {
-                        JSONObject threadResponse = Requests.getResponse(url, request,"POST");
-                        return threadResponse.toString();
-                    }
-                });
-
-
-                new Thread(task).start();
-                Log.i("Response", task.get());
-
-                /* show all users clickable */
-
-                response = new JSONObject(task.get());
-
-                if (response.has("foundUser")) {
-                    String statusEv = (String) response.get("foundUser");
-                    if (statusEv.equals("unsuccessful")) {
-                        listUsers(new JSONArray(), v);
-                        Toast.makeText(v.getContext(), "No such User.", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-
-                    Log.i("allUser:", response.toString());
-                    listUsers(response.getJSONArray("user"), v);
+                JSONObject curr = users.getJSONObject(i);
+                if (curr.has("friendstatus") && curr.getString("friendstatus").equals("friends")) {
+                    //is already shown in friendslist
+                    //do nothing
+                    //arrayUsers.add(new BasicUser(curr.getLong("id"), curr.getString("first_name"), curr.getString("last_name"), curr.getString("email"), true));
+                } else {
+                    arrayUsers.add(new BasicUser(curr.getLong("id"), curr.getString("first_name"), curr.getString("last_name"), curr.getString("email")));
                 }
-
-            } catch (Exception e) {
-                Log.i("Exception --- not requested", e.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-
         }
-
-        private void listUsers(JSONArray users, View view){
-            ArrayList<BasicUser> arrayUsers = new ArrayList<>();
-            OrderedRealmCollection<Friend> friends = ShowFriendsFragment.this.showFriendsListRecyclerViewAdapter.getData();
-
-            for(int i=0; i<users.length(); i++){
-
-                try {
-                    JSONObject curr = users.getJSONObject(i);
-                    if(curr.has("friendstatus") && curr.getString("friendstatus").equals("friends")){
-                        //is already shown in friendslist
-                        //do nothing
-                        //arrayUsers.add(new BasicUser(curr.getLong("id"), curr.getString("first_name"), curr.getString("last_name"), curr.getString("email"), true));
-                    }else {
-                        arrayUsers.add(new BasicUser(curr.getLong("id"), curr.getString("first_name"), curr.getString("last_name"), curr.getString("email")));
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            UserAdapter userAdapter = new UserAdapter(view.getContext(), arrayUsers, showFriendsRecyclerView, showFriendsListRecyclerViewAdapter);
-            listView.setAdapter(userAdapter);
-            listView.setOnItemClickListener(userAdapter);
-        }
+        UserAdapter userAdapter = new UserAdapter(context, arrayUsers, showFriendsRecyclerView, showFriendsListRecyclerViewAdapter);
+        listView.setAdapter(userAdapter);
+        listView.setOnItemClickListener(userAdapter);
     }
 
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        String url = "searchUser";
+
+        try {
+            JSONObject response;
+
+            JSONObject request = new JSONObject();
+            request.put("id", SaveSharedPreference.getUserID(context));
+            request.put("input", query);
+
+            FutureTask<String> task = new FutureTask(new Callable<String>() {
+                public String call() {
+                    JSONObject threadResponse = Requests.getResponse(url, request, "POST");
+                    return threadResponse.toString();
+                }
+            });
+
+
+            new Thread(task).start();
+            Log.i("Response", task.get());
+
+            /* show all users clickable */
+
+            response = new JSONObject(task.get());
+
+            if (response.has("foundUser")) {
+                String statusEv = (String) response.get("foundUser");
+                if (statusEv.equals("unsuccessful")) {
+                    listUsers(new JSONArray());
+                    Toast.makeText(context, "No such User.", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+
+                Log.i("allUser:", response.toString());
+                listUsers(response.getJSONArray("user"));
+            }
+
+        } catch (Exception e) {
+            Log.i("Exception --- not requested", e.toString());
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
 }
