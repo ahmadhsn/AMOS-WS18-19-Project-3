3d9121631027c7cb8ca50c85f0749ca30c2064eb
Waleed Butt <waleedshahdin@gmail.com>
2018-12-19 01:56:38 +0100

    Showing values for filters

--- AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/FragmentActivity/ShowEventsFragment.java
+++ AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/FragmentActivity/ShowEventsFragment.java
@@ -8,10 +8,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -22,6 +24,9 @@ import com.gr03.amos.bikerapp.R;
 import com.gr03.amos.bikerapp.Requests;
 import com.gr03.amos.bikerapp.ShowEventRecylerViewAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.realm.Realm;
 import io.realm.RealmResults;
 
@@ -30,10 +35,11 @@ public class ShowEventsFragment extends Fragment {
     RecyclerView showEventsRecyclerView;
     ShowEventRecylerViewAdapter showEventRecylerViewAdapter;
     private ImageView eventFilterImage;
+    private Spinner countries;
+    private Spinner cities;
 
-    private TextView resultText;
-
-
+    List<String> country = new ArrayList<>();
+    List<String> city = new ArrayList<>();
 
     public ShowEventsFragment() {
     }
@@ -54,9 +60,7 @@ public class ShowEventsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_show_events, container, false);
 
         eventFilterImage = view.findViewById(R.id.event_filter);
-        resultText = view.findViewById(R.id.result);
 
-        eventFilterImage.setOnClickListener(v -> showInputDialog());
 
         Realm.init(container.getContext());
         Realm realm = Realm.getDefaultInstance();
@@ -64,10 +68,25 @@ public class ShowEventsFragment extends Fragment {
         Requests.getJsonResponse("getEvents", container.getContext());
         RealmResults<Event> events = realm.where(Event.class).findAll();
 
+        RealmResults<Address> countries = realm.where(Address.class).distinct("country").findAll();
+        RealmResults<Address> cities = realm.where(Address.class).distinct("city").findAll();
+
+        for (Address address : countries) {
+            country.add(address.getCountry());
+        }
+
+        for (Address address : cities) {
+            city.add(address.getCity());
+        }
+
+
         showEventsRecyclerView = view.findViewById(R.id.showEvents);
         showEventsRecyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
         showEventRecylerViewAdapter = new ShowEventRecylerViewAdapter(container.getContext(), events);
         showEventsRecyclerView.setAdapter(showEventRecylerViewAdapter);
+
+
+        eventFilterImage.setOnClickListener(v -> showInputDialog());
 
         return view;
     }
@@ -76,19 +95,28 @@ public class ShowEventsFragment extends Fragment {
 
         LayoutInflater layoutInflater = LayoutInflater.from(getContext());
         View promptView = layoutInflater.inflate(R.layout.event_filter_dialog, null);
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
-        alertDialogBuilder.setView(promptView);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext(), R.style.MyDialogTheme);
+        countries = promptView.findViewById(R.id.country_spinner);
+        cities = promptView.findViewById(R.id.city_spinner);
 
-        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
-        // setup a dialog window
-        alertDialogBuilder.setCancelable(false)
-                .setPositiveButton("OK", (dialog, id) -> resultText.setText("Hello, " + editText.getText()))
+        alertDialogBuilder.setView(promptView);
+        alertDialogBuilder.setCancelable(false).setTitle("Select Filter")
+                .setPositiveButton("OK", (dialog, id) -> dialog.dismiss())
                 .setNegativeButton("Cancel",
                         (dialog, id) -> dialog.cancel());
 
-        // create an alert dialog
+
+        ArrayAdapter countryAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, country);
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countries.setAdapter(countryAdapter);
+
+        ArrayAdapter cityAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, city);
+        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cities.setAdapter(cityAdapter);
+
         AlertDialog alert = alertDialogBuilder.create();
         alert.show();
+
     }
 
 
