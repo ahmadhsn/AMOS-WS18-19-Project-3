c4289f292a8b8b564fc99c1033790aa70ad1273b
RoxanaJeney <44262598+RoxanaJeney@users.noreply.github.com>
2019-01-12 17:19:15 +0100

    11-deleteAndUpdateRoute Validation updateRoute

--- AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/EditRouteActivity.java
+++ AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/EditRouteActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -47,8 +48,24 @@ public class EditRouteActivity extends AppCompatActivity {
 
     }
 
+    boolean isTextEmpty(EditText text){
+        CharSequence string = text.getText().toString();
+        return TextUtils.isEmpty(string);
+    }
+
 
     public void updateRoute(View view) throws JSONException {
+
+        if (isTextEmpty(route_name)) {
+            Log.i("VALIDATIONROUTE", "route name is empty");
+            route_name.setError("Please enter a route name");
+            return;
+        }
+        if (isTextEmpty(route_description)) {
+            Log.i("VALIDATIONROUTE", "route description is empty");
+            route_description.setError("Please enter a route discription");
+            return;
+        }
 
         JSONObject json = new JSONObject();
         json.put("id_route", routeId);
@@ -66,14 +83,15 @@ public class EditRouteActivity extends AppCompatActivity {
             });
 
             new Thread(task).start();
-            super.onBackPressed();
             Log.i("Response", task.get());
+            Intent intent = new Intent(this, ShowEventActivity.class);
+            startActivity(intent);
             response = new JSONObject(task.get());
 
             if (response.has("routeUpdate")) {
                 String statusRoute = (String) response.get("routeUpdate");
                 if (statusRoute.equals("successfullUpdation")) {
-                    Toast.makeText(getApplicationContext(), "Successful updated Route.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Successfully updated Route.", Toast.LENGTH_LONG).show();
                 }
                 if (statusRoute.equals("InvalidRequestBody")){
                     Toast.makeText(getApplicationContext(),"Invalid Request Body",Toast.LENGTH_LONG).show();
@@ -82,6 +100,7 @@ public class EditRouteActivity extends AppCompatActivity {
         } catch (Exception e) {
             Log.i("Exception --- not requested", e.toString());
         }
+        super.onBackPressed();
     }
     public void cancel(View view){
         finish();
