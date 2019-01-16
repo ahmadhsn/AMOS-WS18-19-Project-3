87f619f93bf8ca534502130c1ccd900636533d37
Muhammad Ahsan Shahid <ahsan.shahiid@gmail.com>
2019-01-16 01:22:52 +0100

    Done with conversation list.

---
+++ AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/Adapters/ShowConversationWithFriendsListRecyclerViewAdapter.java
@@ -0,0 +1,125 @@
+package com.gr03.amos.bikerapp.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gr03.amos.bikerapp.ChatActivity;
+import com.gr03.amos.bikerapp.Models.Friend;
+import com.gr03.amos.bikerapp.Models.Message;
+import com.gr03.amos.bikerapp.R;
+import com.gr03.amos.bikerapp.Requests;
+import com.gr03.amos.bikerapp.SaveSharedPreference;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmRecyclerViewAdapter;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+public class ShowConversationWithFriendsListRecyclerViewAdapter extends RealmRecyclerViewAdapter<Friend, ShowConversationWithFriendsListRecyclerViewAdapter.ViewHolder> {
+    private RealmResults<Friend> mData;
+    private LayoutInflater mInflater;
+    private Context context;
+
+    // data is passed into the constructor
+    public ShowConversationWithFriendsListRecyclerViewAdapter(Context context, RealmResults<Friend> data) {
+        super(data, true);
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.show_my_conversation_list, parent, false);
+        return new ShowConversationWithFriendsListRecyclerViewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ArrayList<Integer> chatUser = new ArrayList<>();
+        chatUser.add(SaveSharedPreference.getUserID(context));
+        chatUser.add(Math.toIntExact(mData.get(position).getId()));
+        int chatId = loadChat(chatUser);
+        Log.i("before chat", String.valueOf(chatId));
+        Realm.init(context);
+        Realm realm = Realm.getDefaultInstance();
+        Message message = realm.where(Message.class).equalTo("id_chat", chatId).sort("time_created", Sort.DESCENDING).findFirst();
+
+        holder.friendName.setText(mData.get(position).getFirst_name());
+        holder.timeStamp.setText(message.getTime_created());
+        holder.lastMessage.setText(message.getMessage());
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, ChatActivity.class);
+            intent.putExtra("chatUser", chatUser);
+            Log.i("this chat", String.valueOf(chatUser));
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    private int loadChat(ArrayList<Integer> chatUser) {
+        JSONObject request = new JSONObject();
+
+        try {
+            JSONArray jsonUserIds = new JSONArray();
+            for (int userId : chatUser) {
+                jsonUserIds.put(userId);
+            }
+            request.put("id_users", jsonUserIds);
+
+            JSONObject response = Requests.getJSONResponse("loadChat", request, "PUT");
+
+            if (response.has("id_chat")) {
+                return response.getInt("id_chat");
+            } else {
+                Toast.makeText(context, "Error loading chat", Toast.LENGTH_LONG).show();
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView friendName;
+        TextView timeStamp;
+        TextView lastMessage;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            friendName = itemView.findViewById(R.id.friend_name);
+            timeStamp = itemView.findViewById(R.id.last_message_time);
+            lastMessage = itemView.findViewById(R.id.last_message);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+        }
+    }
+
+}
--- AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/FragmentActivity/MyChatListFragment.java
+++ AndroidApplikation/app/src/main/java/com/gr03/amos/bikerapp/FragmentActivity/MyChatListFragment.java
@@ -4,26 +4,23 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gr03.amos.bikerapp.Adapters.MessageListRecyclerViewAdapter;
-import com.gr03.amos.bikerapp.Adapters.ShowMyEventRecyclerViewAdapter;
-import com.gr03.amos.bikerapp.Models.Event;
-import com.gr03.amos.bikerapp.Models.EventParticipation;
+import com.gr03.amos.bikerapp.Adapters.ShowConversationWithFriendsListRecyclerViewAdapter;
+import com.gr03.amos.bikerapp.Models.Friend;
 import com.gr03.amos.bikerapp.Models.Message;
 import com.gr03.amos.bikerapp.R;
 
 import io.realm.Realm;
-import io.realm.RealmList;
 import io.realm.RealmResults;
 
 public class MyChatListFragment extends Fragment {
 
     RecyclerView messageListRecyclerViewAdapter;
-    MessageListRecyclerViewAdapter showMessageListRecyclerViewAdapter;
+    ShowConversationWithFriendsListRecyclerViewAdapter showConversationWithFriendsListRecyclerViewAdapter;
     private View view;
     Message message;
 
@@ -47,12 +44,11 @@ public class MyChatListFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_my_chat_list, container, false);
         Realm.init(container.getContext());
         Realm realm = Realm.getDefaultInstance();
+        RealmResults<Friend> friends = realm.where(Friend.class).findAll();
 
-        /* TODO
+      /*  TODO
 
-        RealmResults<EventParticipation> eventParticipations = realm.where(EventParticipation.class).findAll();
-
-        RealmList<Event> events = new RealmList<>();
+               RealmList<Event> events = new RealmList<>();
         for (EventParticipation eventParticipation : eventParticipations) {
             message = realm.where(Event.class)
                     .equalTo("id_event", eventParticipation.getId_event())
@@ -63,15 +59,15 @@ public class MyChatListFragment extends Fragment {
 
         populateRecyclerView(events);
         */
-
+        populateRecyclerView(friends);
         return view;
     }
 
-    private void populateRecyclerView(RealmResults<Message> messages) {
+    private void populateRecyclerView(RealmResults<Friend> friendConversation) {
         messageListRecyclerViewAdapter = view.findViewById(R.id.myChats);
         messageListRecyclerViewAdapter.setLayoutManager(new LinearLayoutManager(getContext()));
-        showMessageListRecyclerViewAdapter = new MessageListRecyclerViewAdapter(getContext(), messages);
-        messageListRecyclerViewAdapter.setAdapter(showMessageListRecyclerViewAdapter);
+        showConversationWithFriendsListRecyclerViewAdapter = new ShowConversationWithFriendsListRecyclerViewAdapter(getContext(), friendConversation);
+        messageListRecyclerViewAdapter.setAdapter(showConversationWithFriendsListRecyclerViewAdapter);
     }
 
 }
